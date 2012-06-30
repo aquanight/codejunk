6aa29de00d9994512bb0596a64540e1a6bac9f44
Thomas Stagner <aquanight@gmail.com>
2012-06-29 20:32:26 -0600

    Add support for %a. Uses Lua 5.2's method if available, else a handmade solution.

--- lua/extformat.lua
+++ lua/extformat.lua
@@ -4,6 +4,8 @@
 -- - For %n, the number of characters stored up to that point is provided as 2nd, 3rd, etc return values from format(),
 --   after the formatted string.
 -- - The length modifiers - h, hh, l, ll, L, j, z, and t - have no effect and as such are simply ignored.
+-- - All numeric formats expect a Lua number. There is no distinction between integer and floating point types, however
+--   integer-based specifiers may cause rounding or truncation of the value.
 -- - Support for %q, which functions as %s but quotes the string and escapes any special characters.
 -- - <N>$ constructs are permitted to leave gaps, since lua does not have the restriction of trying to advance over unknown types,
 --   however, this lua implementation still requires all parameters to have explicit indexes if one is used.
@@ -24,6 +26,80 @@ local error = error;
 local unpack = unpack or table.unpack;
 local dgmt = debug.getmetatable;
 local tostring = tostring;
+
+local pcta;
+-- The only way to distinguish positive and negative zero:
+local pzero = 0;
+local nzero = -pzero; -- Due to constant-folding, -0 doesn't work.
+local pinf = 1/pzero;
+local ninf = 1/nzero;
+if not _G._VERSION:match("Lua 5%.2") then
+	pcta = function(val, width, prec, flags, upper)
+		local exp, mant, sign;
+		local str;
+		if val == 0 then
+			-- Value is 0,
+			exp, mant = 0, 0;
+			if 1/val == pinf then
+				-- Positive 0
+				sign = flags:match("[ +]") or "";
+			else
+				assert(1/val == ninf);
+				-- Negative 0
+				sign = '-';
+			end
+		else
+			if val < 0 then
+				sign = '-';
+				mant = -val;
+			else
+				sign = flags:match("[ +]") or "";
+				mant = val;
+			end
+			exp = 0;
+			repeat
+				local ip, _ = math.modf(mant);
+				if ip < 1 then
+					mant = mant * 2;
+					exp = exp - 1;
+				elseif ip >= 16 then
+					mant = mant / 2;
+					exp = exp + 1;
+				end
+			until (mant >= 1 and mant < 16)
+			while mant ~= 0 do
+				local ip, fp = math.modf(mant);
+				if str then
+					str = str .. _fmt(upper and "%1.1X" or "%1.1x", ip);
+				else
+					str = _fmt(upper and "%1.1X" or "%1.1x.", ip);
+				end
+				if prec then
+					prec = prec - 1
+					if prec < 1 then
+						break
+					end
+				end
+				mant = fp*16;
+			end
+		end
+		str = str .. (upper and "P" or "p") .. _fmt("%d", exp);
+		if width and (#str + 2) < width then
+			local fill = width - (#str + 2);
+			if flags:match("-") then
+				str = "0x" .. str .. (" "):rep(fill);
+			elseif flags:match("0") then
+				str = "0x" .. ("0"):rep(fill) .. str;
+			else
+				str = (" "):rep(fill) .. "0x" .. str;
+			end
+		else
+			str = "0x" .. str;
+		end
+		return str;
+	end
+end
+
 function string.format(fmt, ...)
 	local arg = { n = select("#", ...), ... };
 	local argi = 1;
@@ -76,7 +152,7 @@ function string.format(fmt, ...)
 			end
 			local prec = fmt:match("^%.(%d+)", ix) or fmt:match("^%.(%*%d+%$", ix) or fmt:match("^%.%*", ix);
 			if prec ~= nil then
-				ix = ix + #prec;
+				ix = ix + 1 + #prec;
 				if prec:match("^%*%d+%$") then
 					assert(argi == 1 or argi == nil, "Cannot mix explicit position specifiers with sequenced specifiers");
 					argi = nil;
@@ -85,6 +161,10 @@ function string.format(fmt, ...)
 				elseif prec:match("^%*") then
 					assert(argi ~= nil, "Cannot mix explicit position specifiers with sequences specifiers");
 					prec = assert(tonumber(arg[argspec]), _fmt("bad argument #%d to 'format' (number expected, got %s)", argspec, type(arg[argspec])));
+					if prec < 0 then
+						prec = -prec;
+						flags = flags:match("-") and flags or (flags .. "-");
+					end
 					argspec = argspec + 1;
 					argi = argi + 1;
 				else
@@ -106,6 +186,20 @@ function string.format(fmt, ...)
 				-- Defer these to lua's formatter.
 				spec = spec .. conv;
 				fmtarg = arg[argspec];
+			elseif conv == "a" or conv == "A" then
+				if _G._VERSION:match("Lua 5%.2") then
+					spec = spec .. conv
+					fmtarg = arg[argspec];
+				else
+					if arg ~= arg or arg == pinf or arg == ninf then
+						-- NaN or infintiy
+						spec = spec .. "g";
+						fmtarg = arg[argspec];
+					else
+						spec = "%s";
+						fmtarg = pcta(arg[argspec], width, prec, flag, conv == "A");
+					end
+				end
 			elseif conv == "p" or conv == "P" then
 				-- Format a table, userdata, or function to its address.
 				if spec:find("#", 1, true) == nil then
