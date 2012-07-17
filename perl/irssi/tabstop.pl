63d39338bb43696560b4cbf1c1bf4742a1b4cde2
Thomas Stagner <aquanight@gmail.com>
2012-07-17 13:39:31 -0600

    Leave excess backspaces alone. Also backspace formatting does not cross an 'ident' format specifier.

--- perl/irssi/tabstop.pl
+++ perl/irssi/tabstop.pl
@@ -30,18 +30,20 @@ sub soft_tabs($$\$)
 }
 
 our $_state; # MUST be 'our' for 'local' to work.
+our $lock;
 sub sig_print_text($$)
 {
 	my ($dst, $txt, $stripped) = @_;
 
+	return if $lock;
+
 	my $rewrite = 0;
 
-	if ($txt =~ /\t/)
+	# 0 or less = disabled.
+	my $ts = Irssi::settings_get_int('tabstop_interval');
+	if ($ts >= 1 && $txt =~ /\t/)
 	{
 		# ABSOLUTELY MUST NOT EVER PRINT A TAB HERE!!!
-		my $ts = Irssi::settings_get_int('tabstop_interval');
-		# 0 or less = disabled.
-		return if ($ts < 1);
 		if (Irssi::settings_get_bool("tabstop_fixedtabs"))
 		{
 			my $spc = ' ' x $ts;
@@ -62,7 +64,7 @@ sub sig_print_text($$)
 		$rewrite = 1;
 	}
 
-	if ($txt =~ /\cH/)
+	if (Irssi::settings_get_bool("interpret_backspace") && $txt =~ /\cH/)
 	{
 		# Must handle UTF-8 characters correctly.
 		if (Irssi::settings_get_str("term_charset") eq "utf-8")
@@ -70,7 +72,12 @@ sub sig_print_text($$)
 			utf8::decode($txt);
 		}
 		$_state = 0;
-		$txt =~ s/((?:\cB|\c_|\cV|\cO|\cF|\cC\d*,\d*|\cD[abcdeghi]|\cD..|\e\[[[:digit:];]*m)*(?:.(?{local $_state = $_state + 1}))+)(??{sprintf("%c{%d}", 8, $_state)})/sprintf("\cDg\cD4\/%s\cDg", Irssi::strip_codes($1))/ge;
+		my @segments = split /\cDe/, $txt, -1;
+		for my $segment (@segments)
+		{
+			$segment =~ s/((?:\cB|\c_|\cV|\cO|\cF|\cC\d*,\d*|\cD[abcdeghi]|\cD..|\e\[[[:digit:];]*m)*(?:.(?{local $_state = $_state + 1}))+)(??{sprintf("%c{%d}", 8, $_state)})/sprintf("\cDg\cD4\/%s\cDg", Irssi::strip_codes($1))/ge;
+		}
+		$txt = join "\cDe", @segments;
 		$rewrite = 1;
 		if (Irssi::settings_get_str("term_charset") eq "utf-8")
 		{
@@ -80,12 +87,15 @@ sub sig_print_text($$)
 
 	if ($rewrite)
 	{
+		$lock = 1;
 		Irssi::signal_emit("print text", $dst, $txt, $stripped);
+		$lock = 0;
 		Irssi::signal_stop();
 	}
 }
 
 Irssi::settings_add_bool(tabstop => "tabstop_fixedtabs", 0); # 1 -> tabs are always the same length.
 Irssi::settings_add_int(tabstop => "tabstop_interval", 9); # Length of fixed tab, or interval of tab stops.
+Irssi::settings_add_bool(tabstop => "interpret_backspace", 1); # 1 -> enable backspace handling
 
 Irssi::signal_add("print text" => \&sig_print_text);
